9dae2ac247e8bc00d2485ec618fb399b7221fa99
Даниил Попугаев <daniilapopugaev@yandex.ru>
2025-11-09 00:46:30 +0300

    add record point

---
+++ src/Models/Point.cs
@@ -0,0 +1,11 @@
+﻿namespace Labyrinths.Models;
+
+// Запись для представления точки в лабиринте
+record Point(int X, int Y)
+{
+    public static Point Parse(string s)
+    {
+        var parts = s.Split(',');
+        return parts.Length != 2 ? throw new FormatException("Expect format x,y") : new Point(int.Parse(parts[0]), int.Parse(parts[1]));
+    }
+}
