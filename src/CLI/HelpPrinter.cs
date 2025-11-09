54faa72d7ee141071cd5881c6909f3f684471156
Даниил Попугаев <daniilapopugaev@yandex.ru>
2025-11-10 00:20:51 +0300

    fix help printer

--- src/CLI/HelpPrinter.cs
+++ src/CLI/HelpPrinter.cs
@@ -14,13 +14,10 @@ public static class HelpPrinter
     {
         Console.WriteLine("Usage: maze-app [-hV] [COMMAND]");
         Console.WriteLine("Maze generator and solver CLI application.");
-        Console.WriteLine();
-        Console.WriteLine("Options:");
         Console.WriteLine("  -h, --help      Show this help message and exit.");
         Console.WriteLine("  -V, --version   Print version information and exit.");
-        Console.WriteLine();
         Console.WriteLine("Commands:");
-        Console.WriteLine("  generate        Generate a maze with specified algorithm and dimensions.");
-        Console.WriteLine("  solve           Solve a maze with specified algorithm and points.");
+        Console.WriteLine("  generate  Generate a maze with specified algorithm and dimensions.");
+        Console.WriteLine("  solve     Solve a maze with specified algorithm and points.");
     }
 }
